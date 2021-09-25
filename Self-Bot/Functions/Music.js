2f0a38f36972aa937a15d3ea52314f900c264001
1e17 <68991107+1e17@users.noreply.github.com>
2021-09-24 17:34:51 -0700

    Add files via upload

---
+++ Self-Bot/Functions/Music.js
@@ -0,0 +1,100 @@
+
+// Packages
+const 
+  Ytdl = require('ytdl-core')
+  YtSearch = require('yt-search')
+
+// Music Functions
+const 
+  Singer = {
+    Singing: false,
+    Queue: []
+  }
+
+  Play = async ()=>{
+    const Song = Singer.Queue[0]
+    Singer.Singing = true 
+
+    Song.Voice.join().then(function(Dispatcher){
+      Song.Channel.send(`🎶 Now playing: **${Song.Title}**. Requested by: **${Song.Asker.username}.**`)
+      Singer.Queue[0] = {Constructor: Song, Dispatcher: Dispatcher.play(Song.Audio).on('finish', function(){
+        Singer.Queue.shift()
+        if (Singer.Queue.length > 0){Play()} else {Singer.Singing = false}
+      })}
+    })
+  }
+
+  Get_Info = async function(Song){
+    if (Ytdl.validateURL(Song)) {
+      return await Ytdl.getInfo(Song)
+    } else {
+      const Result = await YtSearch(Song)
+      if (Result.videos.length > 0) {
+        return await Ytdl.getInfo(Result.videos[0].url)
+      } else {
+        console.log('Videos not found.')
+      }
+    }
+  }
+
+  Queue_Song = async function(SongData){
+    const VideoInfo = await Get_Info(SongData.Song)
+    const Audio = Ytdl(VideoInfo.videoDetails.video_url, {filter: 'audioonly'})
+    const Constructor = {
+      Audio: Audio,
+      Channel: SongData.Channel,
+      Asker: SongData.Author,
+      Voice: SongData.Voice,
+      Title: VideoInfo.videoDetails.title,
+    }
+
+    SongData.Channel.send(`Added **${Constructor.Title}** to queue.`)
+    Singer.Queue.push(Constructor)
+    if (!Singer.Singing) {Play().catch((Fuck) => {console.log('Play Function: ' + Fuck)})}
+  }
+
+  Shuffle_Array = function(array) { 
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array
+  }
+  
+
+module.exports = {
+  Handle_Play: async function(Data){
+    const 
+      Song = Data.Args.join(' ')
+      Channel = Data.Channel
+      VoiceChannel = Data.Voice
+
+    if (!Song){return Channel.send('No song provided.')} 
+    if (!VoiceChannel){return Channel.send('Not in voice channel.')} 
+
+    Queue_Song({
+      Song: Song,
+      Author: Data.Author,
+      Voice: VoiceChannel,
+      Channel: Channel
+    })
+  },
+  
+  Handle_Skip: async function(){
+    Singer.Queue[0].Constructor.Channel.send('**Skipped.**')
+    Singer.Queue[0].Dispatcher.end()
+  },
+
+  Handle_Shuffle: async function(Data){
+    if (Singer.Queue.length > 0){          
+      const Current = Singer.Queue.shift()
+      Singer.Queue = Shuffle_Array(Singer.Queue)
+      Singer.Queue.unshift(Current)
+      Data.Channel.send('**Shuffled.**')
+    } else {
+      Data.Channel.send('**No songs to shuffle.**')
+    }
+  }
+}
---
+++ Self-Bot/main.js
@@ -0,0 +1,78 @@
+
+// Packages
+const 
+  Discord = require('discord.js-selfbot')
+  Music = require('./Functions/Music.js')
+  Fs = require('fs')
+
+// Config Loader 
+if (!Fs.existsSync('./config.cfg')){return console.log("Config doesn't exist. (▀̿Ĺ̯▀̿ ̿)")}
+let Config = JSON.parse(Fs.readFileSync('./config.cfg'))
+
+// Declarations 
+const 
+  Client = new Discord.Client()
+  Save_Config = ()=>{Fs.writeFileSync('./config.cfg',JSON.stringify(Config,null,2))}
+
+// Functions
+const
+  Array_Remove = (array,item)=>{
+    for (let i=0;i<=array.length;++i){
+      if (array[i] === item){array.splice(i,1)}
+    }
+  }
+
+// Events 
+Client.on('ready', ()=>{
+  console.log('Aux Connected')
+  Client.user.setActivity('ya grandma', {type: 'WATCHING'})
+})
+
+Client.on('message', (Message)=>{
+  if (Message.content.startsWith(Config.PREFIX) && Config.WHITELISTED.includes(Message.author.id)){
+    const [Command, ...Args] = Message.content.split(Config.PREFIX)[1].split(' ')
+
+    switch(Command){
+      // Global Handlers
+      case 'prefix':
+        if(!Args[0]){return Message.channel.send('**No prefix provided.**')}
+        Config.PREFIX = Args[0]
+        Message.channel.send('**Prefix set to ** ``'+Args[0]+'``')
+        Save_Config()
+      break
+
+      case 'whitelist':
+        if(!Args[0]){return Message.channel.send('**No ID provided.**')}
+        if(Config.WHITELISTED.includes(Args[0])){return Message.channel.send('**Already whitelisted.**')}
+        Config.WHITELISTED.push(Args[0].toString())
+        Message.channel.send('**Whitelisted **'+`<@${Args[0]}>.`)
+        Save_Config()
+      break 
+
+      case 'blacklist':
+        if(!Args[0]){return Message.channel.send('No ID provided.')}
+        if(!Config.WHITELISTED.includes(Args[0])){return Message.channel.send("**User wasn't whitelisted.**")}
+        Array_Remove(Config.WHITELISTED,Args[0])
+        Message.channel.send('**User blacklisted.**')
+        Save_Config()
+      break 
+
+      // Music Handlers
+      case 'play':
+        Music.Handle_Play({Author:Message.author,Args:Args,Channel:Message.channel,Voice:Message.member.voice.channel})
+      break
+
+      case 'skip': 
+        Music.Handle_Skip(null) 
+      break
+
+      case 'shuffle':
+        Music.Handle_Shuffle({Channel:Message.channel})
+      break
+    }
+  }
+})
+
+
+// Login 
+Client.login(Config.TOKEN)
