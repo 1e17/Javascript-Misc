8e77c2fd7136100651cf11f1491134c769ecbb9b
1e17 <68991107+1e17@users.noreply.github.com>
2021-09-25 15:38:40 -0700

    Update main.js

--- Self-Bot/main.js
+++ Self-Bot/main.js
@@ -2,8 +2,10 @@
 // Packages
 const 
   Discord = require('discord.js-selfbot') 
+  Fs = require('fs')
   Music = require('./Functions/Music.js')
   ConfigEditor = require('./Functions/ConfigEditor.js')
+  Miscellaneous = require('./Functions/Misc.js')
 
 // Config Check 
 if (Config===null){return}
@@ -16,6 +18,17 @@ const
 // Load Commands 
 for (i of Object.keys(Music)){Commands[i] = Music[i]}
 for (i of Object.keys(ConfigEditor)){Commands[i] = ConfigEditor[i]}
+for (i of Object.keys(Miscellaneous)){Commands[i] = Miscellaneous[i]}
+Commands.loadpackage = (Data) => {
+  const [Args,Channel] = [Data.Args,Data.Channel]
+  if(!Args[0]){return Channel.send('**No package provided.**')}
+  if (!Fs.existsSync('./Functions/'+Args[0])){return Channel.send("**Package doesn't exist.** (▀̿Ĺ̯▀̿ ̿)")}
+  const Export = require('./Functions/'+Args[0])
+  for (i of Object.keys(Export)){
+    Commands[i] = Export[i]
+    Channel.send('**Command Loaded: **'+'``'+Config.PREFIX+i+'``')
+  }
+}
 
 // Events 
 Client.on('ready', ()=> {
