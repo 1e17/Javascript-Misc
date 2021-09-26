b9b9c80ae509393debcef0417a2246b0a0a54b0f
1e17 <68991107+1e17@users.noreply.github.com>
2021-09-25 19:13:20 -0700

    Update main.js

--- Self-Bot/main.js
+++ Self-Bot/main.js
@@ -3,40 +3,31 @@
 const 
   Discord = require('discord.js-selfbot') 
   Fs = require('fs')
-  Music = require('./Functions/Music.js')
-  ConfigEditor = require('./Functions/ConfigEditor.js')
-  Miscellaneous = require('./Functions/Misc.js')
 
-// Config Check 
-if (Config===null){return}
+// Config Loader 
+if (!Fs.existsSync('./config.cfg')){return console.log("Config doesn't exist. (▀̿Ĺ̯▀̿ ̿)")}
+global.Config = JSON.parse(Fs.readFileSync('./config.cfg'))
 
 // Declarations 
-const 
+const
   Client = new Discord.Client()
   Commands = []
+  Load_Command_Set = (Export) => {for (i of Object.keys(Export)){Commands[i] = Export[i]}}
 
 // Load Commands 
-for (i of Object.keys(Music)){Commands[i] = Music[i]}
-for (i of Object.keys(ConfigEditor)){Commands[i] = ConfigEditor[i]}
-for (i of Object.keys(Miscellaneous)){Commands[i] = Miscellaneous[i]}
-Commands.loadpackage = (Data) => {
-  const [Args,Channel] = [Data.Args,Data.Channel]
-  if(!Args[0]){return Channel.send('**No package provided.**')}
-  if (!Fs.existsSync('./Functions/'+Args[0])){return Channel.send("**Package doesn't exist.** (▀̿Ĺ̯▀̿ ̿)")}
-  const Export = require('./Functions/'+Args[0])
-  for (i of Object.keys(Export)){
-    Commands[i] = Export[i]
-    Channel.send('**Command Loaded: **'+'``'+Config.PREFIX+i+'``')
-  }
-}
+Fs.readdir(__dirname+'\\Functions', (Error,Files) => {
+  Files.forEach((File) => {
+    Load_Command_Set(require(__dirname+'\\Functions\\'+File))
+  })
+})
 
 // Events 
-Client.on('ready', ()=> {
-  console.log('Aux Connected')
+Client.on('ready', () => {
+  console.log('Connected')
   Client.user.setActivity('ya grandma', {type: 'WATCHING'})
 })
 
-Client.on('message', (Message)=> {
+Client.on('message', (Message) => {
   if (Message.content.startsWith(Config.PREFIX) && Config.WHITELISTED.includes(Message.author.id)){
     const [Command, ...Args] = Message.content.split(Config.PREFIX)[1].split(' ')
     const [Channel,Voice,Author] = [Message.channel, Message.member.voice.channel, Message.author]
@@ -55,6 +46,7 @@ Client.on('message', (Message)=> {
     }
     
   }
+
 })
 
 // Login 
